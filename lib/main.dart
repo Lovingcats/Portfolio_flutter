8b43183b76cc847e405f6d11648523dbe7e23752
lovingcats <whehdrms6810@naver.com>
2024-06-11 17:50:15 +0900

    [ADD] :: (#2) 1차 기초 디바이스 UI 그리기

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -59,6 +58,61 @@ class DesktopScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: CustomPaint(
+          size: Size(400, 850),
+          painter: DeviceFramePainter(),
+        ),
+      ),
+    );
+  }
+}
+
+class DeviceFramePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5;
+
+    // 디바이스 외곽
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+        Rect.fromLTWH(0, 0, size.width, size.height),
+        Radius.circular(20),
+      ),
+      paint,
+    );
+    
+    // 전원 버튼
+    final powerButtonPaint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.fill;
+
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+        Rect.fromLTWH(size.width, size.height / 2 - 130, 4.5, 70),
+        Radius.circular(5),
+      ),
+      powerButtonPaint,
+    );
+
+    // 볼륨 버튼
+    canvas.drawRRect(
+      RRect.fromRectAndRadius(
+        Rect.fromLTWH(size.width, size.height / 2 - 320, 4.5, 125),
+        Radius.circular(5),
+      ),
+      powerButtonPaint,
+    );
+    
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
