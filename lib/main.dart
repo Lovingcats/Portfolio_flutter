38ee74f1aaeaa41a8f688e2b5b7bb20de55b0e15
lovingcats <whehdrms6810@naver.com>
2024-06-13 13:47:41 +0900

    [ADD] :: (#2) 외각 안쪽에 galaxy s21 배경화면 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart' show rootBundle;
+
 
 void main() {
   runApp(const MyApp());
@@ -53,11 +58,38 @@ class TabletScreen extends StatelessWidget {
   }
 }
 
-class DesktopScreen extends StatelessWidget {
+class DesktopScreen extends StatefulWidget {
   const DesktopScreen({super.key});
 
   @override
+  _DesktopScreenState createState() => _DesktopScreenState();
+}
+
+class _DesktopScreenState extends State<DesktopScreen> {
+  ui.Image? _backgroundImage;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImage();
+  }
+
+  Future<void> _loadImage() async {
+    final ByteData data = await rootBundle.load('assets/img/deviceBackground.png');
+    final List<int> bytes = data.buffer.asUint8List();
+    final ui.Codec codec = await ui.instantiateImageCodec(Uint8List.fromList(bytes));
+    final ui.FrameInfo fi = await codec.getNextFrame();
+    setState(() {
+      _backgroundImage = fi.image;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (_backgroundImage == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
     return Scaffold(
       appBar: null,
       body: Center(
@@ -77,7 +109,7 @@ class DesktopScreen extends StatelessWidget {
 
             return CustomPaint(
               size: Size(deviceWidth, deviceHeight),
-              painter: DeviceFramePainter(),
+              painter: DeviceFramePainter(_backgroundImage!),
             );
           },
         ),
@@ -87,6 +119,10 @@ class DesktopScreen extends StatelessWidget {
 }
 
 class DeviceFramePainter extends CustomPainter {
+  final ui.Image backgroundImage;
+
+  DeviceFramePainter(this.backgroundImage);
+
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
@@ -99,7 +135,7 @@ class DeviceFramePainter extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 10;
 
-    // 디바이스 외곽
+    // Device outer border
     canvas.drawRRect(
       RRect.fromRectAndRadius(
         Rect.fromLTWH(0, 0, size.width, size.height),
@@ -108,7 +144,7 @@ class DeviceFramePainter extends CustomPainter {
       paint,
     );
 
-    // 파란색 외곽 테두리
+    // Blue inner border
     canvas.drawRRect(
       RRect.fromRectAndRadius(
         Rect.fromLTWH(5, 5, size.width - 10, size.height - 10),
@@ -117,7 +153,11 @@ class DeviceFramePainter extends CustomPainter {
       bluePaint,
     );
 
-    // 전원 버튼
+    // Draw background image
+    final imageRect = Rect.fromLTWH(5, 5, size.width - 10, size.height - 10);
+    canvas.drawImageRect(backgroundImage, Rect.fromLTWH(0, 0, backgroundImage.width.toDouble(), backgroundImage.height.toDouble()), imageRect, paint);
+
+    // Power button
     final powerButtonPaint = Paint()
       ..color = Colors.black
       ..style = PaintingStyle.fill;
@@ -130,7 +170,7 @@ class DeviceFramePainter extends CustomPainter {
       powerButtonPaint,
     );
 
-    // 볼륨 버튼
+    // Volume button
     canvas.drawRRect(
       RRect.fromRectAndRadius(
         Rect.fromLTWH(size.width, size.height / 2 - 320, 4.5, 125),
