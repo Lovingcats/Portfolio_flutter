0311041e5863fa7f318f5fcb481e611aff5fb52f
lovingcats <whehdrms6810@naver.com>
2024-06-14 13:42:47 +0900

    [ADD] :: (#2) 배터리 위치 수정

--- lib/widget/deviceframepainter.dart
+++ lib/widget/deviceframepainter.dart
@@ -126,14 +126,14 @@ class DeviceFramePainter extends CustomPainter {
 
     canvas.drawRRect(
       RRect.fromRectAndRadius(
-        Rect.fromLTWH(-12, -20, 13, 8), // 가로형 배터리
+        Rect.fromLTWH(-12, -10, 13, 8), // 가로형 배터리
         const Radius.circular(1.9),
       ),
       batteryPaint,
     );
 
     canvas.drawRect(
-      Rect.fromLTWH(-12, -20, 8, 8), // 가로형 배터리 내부
+      Rect.fromLTWH(-12, -10, 8, 8), // 가로형 배터리 내부
       batteryLevelPaint,
     );
 
