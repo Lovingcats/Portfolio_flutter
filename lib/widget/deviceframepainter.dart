131fa63b819f071803e2489f7e915ec69d0b6a8b
lovingcats <whehdrms6810@naver.com>
2024-06-26 13:04:16 +0900

    [DELETE] :: (#33) 불필요한 import 제거

--- lib/widget/deviceframepainter.dart
+++ lib/widget/deviceframepainter.dart
@@ -1,6 +1,5 @@
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 
 class DeviceFramePainter extends CustomPainter {
   final ui.Image backgroundImage;
